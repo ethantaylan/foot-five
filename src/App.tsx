592dd343b4812759fa782b4a74aa920db940ee0a
dev <ethtaylan@gmail.com>
2024-05-31 13:08:19 +0200

    Fix build

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import "./App.css";
 import { AppLayout } from "./components/AppLayout";
 import ClerkLogin from "./components/ClerkLogin";
 
-import { Login } from "./components/Login";
-
 export default function App() {
   return (
     <AppLayout>
