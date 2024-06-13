9396e3f843521b65e52060d72c5a537deefafa8a
Ethan TAYLAN <ethantaylan@MacBook-Pro-de-Ethan.local>
2024-06-13 19:28:06 +0200

    feat: added time in home

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,37 @@
 import { ArrowLeftIcon } from "@heroicons/react/16/solid";
 import Profil from "../Profil/Profil";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const { pathname } = useLocation();
+  const [date, setDate] = useState<string>("");
   const navigate = useNavigate();
+
+  const formatDate = (date: Date) => {
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const secondes = String(date.getSeconds()).padStart(2, "0");
+    return `${day}/${month}/${year} - ${hours}:${minutes}:${secondes}`;
+  };
+
+  useEffect(() => {
+    const updateDate = () => {
+      const currentDate = new Date();
+      setDate(formatDate(currentDate));
+    };
+
+    const intervalId = setInterval(updateDate, 1000);
+
+    // Initial date update
+    updateDate();
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div className="flex mb-2 justify-between">
@@ -15,7 +42,10 @@ export default function Header() {
         />
       )}
       <button className="font-black text-xl">FIVE ⚽</button>
-      <Profil />
+      <div className="flex items-center">
+        <span className="text-xs me-2 badge bg-opacity-65 rounded border shadow-md">{date}</span>
+        <Profil />
+      </div>
     </div>
   );
 }
